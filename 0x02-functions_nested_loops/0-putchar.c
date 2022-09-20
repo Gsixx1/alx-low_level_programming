f02357628426d84e51ce1aefe13c4117849568bd
Gsixx1 <hssnknnh@gmail.com>
2022-09-20 15:55:00 -0700

    two files added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - this is the name of the function
+ * and all codes are within the scope of this function.
+ *
+ * Description: This program is just to print the
+ * string [ _putchar ].
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
