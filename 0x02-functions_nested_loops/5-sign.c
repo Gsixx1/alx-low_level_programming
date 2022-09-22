462f4f7d8210dc6bce5754943af9450f9122e7d5
Gsixx1 <hssnknnh@gmail.com>
2022-09-22 01:07:30 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ * @n: function parameter or the argument of the function.
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+')
+			return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+	}
+	else
+	{
+		_putchar('0);
+		return (0);
+	}
+}
