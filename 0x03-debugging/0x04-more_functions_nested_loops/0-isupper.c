8579769efb94e14c068817547c110cb1e1c77931
Gsixx1 <hssnknnh@gmail.com>
2022-09-22 16:11:02 -0700

    grave mistake

--- 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks if a character is uppercase or not
  * @c: character to be tested, its a parameter.
- * Return: 1, otherwise 0.
+ * Return: 1, otherwise 0.. YES.
  */
 
 int _isupper(int c)
