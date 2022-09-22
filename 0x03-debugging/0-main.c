0130b48132bd968e62440f8322d9869ff8830da7
Gsixx1 <hssnknnh@gmail.com>
2022-09-22 14:55:27 -0700

    Description added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,6 +4,9 @@
  * main - this tests the function that prints if
  * integer is positive or negative.
  *
+ * Description: This project only needs to be debugged, and
+ * the output will print the code.
+ *
  * Return: 0
  */
 int main(void)
