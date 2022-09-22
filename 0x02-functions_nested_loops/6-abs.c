0160028aab6c8396715c6a522698738b47fad0d5
Gsixx1 <hssnknnh@gmail.com>
2022-09-22 01:59:21 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - this is the name of the function
+ * it checks the code.
+ *
+ * @c: thisi is the int parameter that will
+ * be used for the argument of the function.
+ *
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || C == 0)
+	{
+		return (c);
+	}
+	else
+		return (c * -1);
+}
