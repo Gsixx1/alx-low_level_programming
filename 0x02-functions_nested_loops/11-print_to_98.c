16025e96170ffdf23785066e6b8dd5a56af6af57
Gsixx1 <hssnknnh@gmail.com>
2022-09-22 09:51:36 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - This is the function name and it prints all the
+ * natural numbers from inputi(n) to 98 in order seperated by a comma
+ * followed by a space.
+ *
+ * @n: This is the parameter of the function with data type int.
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+	printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+				printf("\n", n);
+	}
+}
